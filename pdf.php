141c74db6b9a03e5e8e5ff0bd26470822041a01c
sufyan <mdsufyan7@gmail.com>
2022-05-31 21:47:46 +0530

    Update pdf.php
    Table size reduced
    Separate tables for each category
    Single image in a table

--- pdf.php
+++ pdf.php
@@ -3,15 +3,26 @@ function print_pdf_bkh(){
     ?>
     <style type="text/css">
         body{
-            max-width: 1200px;
+            max-width: 1000px;
             margin: auto;
-            padding: 20px;
+            padding: 10px;
         }
-        table,td{
+        th,table{
+            border-bottom: 1px solid;
+            border-top: 1px solid;
             border-collapse: collapse;
         }
+        table{
+            width: 100%;
+        }
         td,th{
-            padding: 10px;
+            font-size: 15px;
+        }
+        .cat_name{
+            font-size: 20px;
+            font-weight: bold;
+        }
+        td,th,p{
             font-family: sans-serif;
         }
         button{
@@ -22,75 +33,105 @@ function print_pdf_bkh(){
             button{
                 display: none;
             }
+            .pagebreak{
+                /*page-break-after: always;*/
+            }
         }
     </style>
-        <?php echo apply_filters('the_content',get_option('product_table_header'),'features',true); ?>
-    <center>
-        <br><button onclick="window.print()"><big>Print / Save as PDF</big></button>
-    </center>
-    <table border="1">
+    <?php echo apply_filters('the_content',get_option('product_table_header'),'features',true); ?>
+        <caption>
+            <button onclick="window.print()"><big>Print / Save as PDF</big></button>
+        </caption>
+    <?php
+    $args = array(
+        'taxonomy'     => 'product_cat',
+        'orderby'      => 'name',
+        'show_count'   => 0,
+        'pad_counts'   => 0,
+        'hierarchical' => 1,
+        'title_li'     => '',
+        'hide_empty'   => 1
+    );
+    $categories = get_categories( $args );
+    foreach ($categories as $category) {
+        echo '<p class="cat_name">'.$category->name.'</p>';
+        ?>
+        <table>
         <thead>
             <tr>
                 <th>Producer</th>
-                <!-- <th>Regions/Producers</th> -->
                 <th>Name</th>
                 <th>SKU</th>
                 <th>Format</th>
-                <!-- <th>Summary</th> -->
                 <th>Price</th>
-                <th>Sale Price</th>
-                <!-- <th>Notes</th> -->
             </tr>
         </thead>
         <tbody>
+        <?php
+        $args = array(
+            'post_type'      => 'product',
+            'post_status'    => 'publish',
+            'posts_per_page' => -1,
+            'tax_query' => array(
+                array(
+                'taxonomy' => 'product_cat',
+                'field'     => 'id', 
+                'terms'     =>  $category->term_id,
+                'operator'  => 'IN'
+                ),
+                array(
+                    'taxonomy'      => 'product_visibility',
+                    'field'         => 'slug',
+                    'terms'         => 'exclude-from-catalog', // Possibly 'exclude-from-search' too
+                    'operator'      => 'NOT IN'
+                )
+            )
+        );
+        $loop = new WP_Query( $args );
+        $sym = get_woocommerce_currency_symbol();
+        $image = 1;
+        while ( $loop->have_posts() ) : $loop->the_post();
+            global $product;
+            $brand = get_post_meta( $product->id, 'brand_logo', true );
+            $brand_url = wp_get_attachment_image_src($brand, 'medium');
+            $cat = get_the_terms ( $product->id, 'product_cat' )[0]->name;
+            $tags = get_the_terms ( $product->id, 'product_tag' );
+            $notes = get_post_meta( $product->id, 'notes', true );
+            echo '<tr>';
+            if ($image==1) {
+                echo '<td rowspan="'.$loop->post_count.'">';
+                if ($brand_url[0]) {
+                    echo '<img src="'.$brand_url[0].'" height="80"></td>';
+                }
+                echo '</td>';
+                $image = 0;
+            }
+            echo '<td>'.get_the_title().'</td>
+            <td>'.$product->get_sku().'</td>
+            <td>';
+            foreach ($tags as $tag) {
+                echo $tag->name.', ';
+            }
+            echo '</td>';
             
-    <?php
-    $args = array(
-        'post_type'      => 'product',
-        'posts_per_page' => -1,
-    );
+            $price = $product->get_price();
+            if ($price) {
+                $price = $sym.$price;
+            }
+            echo '<td>'.$price.'</td>';
+            echo '</tr>';
+        endwhile;
 
-    $loop = new WP_Query( $args );
-    $sym = get_woocommerce_currency_symbol();
-    while ( $loop->have_posts() ) : $loop->the_post();
-        global $product;
-        $brand = get_post_meta( $product->id, 'brand_logo', true );
-        $brand_url = wp_get_attachment_image_src($brand, 'medium');
-        $cat = get_the_terms ( $product->id, 'product_cat' )[0]->name;
-        $tags = get_the_terms ( $product->id, 'product_tag' );
-        $notes = get_post_meta( $product->id, 'notes', true );
-        echo '<tr>
-        <td><img src="'.$brand_url[0].'" height="80"></td>';
-        // echo '<td>'.$cat.'</td>';
-        echo '<td>'.get_the_title().'</td>
-        <td>'.$product->get_sku().'</td>
-        <td>';
-        foreach ($tags as $tag) {
-            echo $tag->name.', ';
-        }
-        echo '</td>';
-        
-        $price = $product->get_regular_price();
-        $sprice = $product->get_sale_price();
-        if ($price) {
-            $price = $sym.$price;
-        }
-        if ($sprice) {
-            $sprice = $sym.$sprice;
-        }
-        // echo '<td>'.get_the_excerpt().'</td>';
-        echo '<td>'.$price.'</td>
-        <td>'.$sprice.'</td>';
-        // echo '<td>'.$notes.'</td>';
-        
-        echo '</tr>';
-    endwhile;
-
-    wp_reset_query();
+        wp_reset_query();
+        ?>
+            </tbody>
+        </table>
+        <div class="pagebreak"></div>
+        <?php
+    }
     ?>
-        </tbody>
-    </table>
-    <center><button onclick="window.print()"><big>Print / Save as PDF</big></button></center>
+    <button onclick="window.print()"><big>Print / Save as PDF</big></button>
+    <?php echo apply_filters('the_content',get_option('product_table_footer'),'features',true); ?>
     <?php
     exit;
 }
