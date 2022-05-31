cae026578a578e2b9c0cc65eeeb3ca23ef4b30b6
sufyan <mdsufyan7@gmail.com>
2022-05-31 21:47:12 +0530

    Update product_table_header.php
    Table Footer added

--- product_table_header.php
+++ product_table_header.php
@@ -12,4 +12,17 @@
 		submit_button('Save Changes', 'primary');
 		?>
 	</form>
+	<h1>Product Table PDF Footer:</h1>
+	<form method="POST">
+		<?php
+		$key = 'product_table_footer';
+		if (isset($_POST[$key])) {
+			update_option($key,stripslashes( $_POST[$key] ));
+		}
+		$html = get_option($key);
+		$settings = array('textarea_name'=>$key);
+		wp_editor($html,$key, $settings);
+		submit_button('Save Changes', 'primary');
+		?>
+	</form>
 </div>
